8eacff401c793993533deee8484a81eae7b697e2
Diego Augusto de Faria Barros <diego@diego-IPMH61R2.(none)>
2012-10-17 15:42:08 -0300

    Criacao DB

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+INFOInt::Application.config.secret_token = '27d3adf5353f1ebe45d961eb711e3590b253abce213c73a23883eadefffcb185d475312ce0a233b5a6fc73265c17483ef4206347f2d9d271e21db0496fd361c5'
